511ae9cef726e7ed8fa9ad136cdbdb68f0756444
unknown <ryan.zhang126@gmail.com>
2023-09-18 19:12:21 -0400

    assign1-2

---
+++ assigns/assign1/MySolution/assign1-2.ml
@@ -0,0 +1,15 @@
+
+#use "./../../../classlib/OCaml/MyOCaml.ml";; 
+let string_merge(cs1, cs2) =
+  let rec helper i j work =
+    if string_length cs1=i then
+      int1_foreach(string_length cs2 - j)(fun x -> work(string_get_at(cs2)(j + x)))
+    else if string_length cs2=j then
+      int1_foreach(string_length cs1 - i)(fun x -> work(string_get_at(cs1) (i + x)))
+    else if ord (string_get_at(cs1) (i)) < ord (string_get_at(cs2) (j)) then
+      (work(string_get_at(cs1) (i)); helper(i+1)(j)work)
+    else
+      (work(string_get_at(cs2) (j)); helper(i)(j+1)work)
+  in
+  string_make_fwork(helper 0 0)
+;;
