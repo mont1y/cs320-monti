6e849e143a785ba56c4189437227db1c59965159
unknown <ryan.zhang126@gmail.com>
2023-09-20 20:49:28 -0400

    ass0

--- assigns/assign0/MySolution/assign0-1.ml
+++ assigns/assign0/MySolution/assign0-1.ml
@@ -4,6 +4,8 @@ let rec fact(x: int): int =
   if x > 0 then x * fact(x-1) else 1;;
 
 let rec
-myans(x:int): int =
-if fact(x) = 0 then x else myans(x+1)
+loop(x:int): int =
+if fact(x) = 0 then x else loop(x+1)
 ;;
+
+val myans = loop(0);;
