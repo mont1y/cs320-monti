68fd418a30a416ddfcfec94c2a97c82924a1a25b
unknown <ryan.zhang126@gmail.com>
2023-09-27 09:11:44 -0400

    test

--- assigns/assign2/MySolution/OCaml/assign2-4.ml
+++ assigns/assign2/MySolution/OCaml/assign2-4.ml
@@ -1,4 +1,21 @@
 #use "./../../../classlib/OCaml/MyOCaml.ml";; 
+
+let rec
+list_foldleft
+(xs: 'a list)
+(r0: 'r0)(fopr: 'r0 -> 'a -> 'r0): 'r0 =
+match xs with
+| [] -> r0
+| (x1 :: xs) ->
+  list_foldleft(xs)(fopr(r0)(x1))(fopr)
+;;
+(* ****** ****** *)
+
+let foldleft_to_iforeach(foldleft: ('xs, 'x0, int) foldleft): ('xs, 'x0) iforeach = 
+  fun(xs)(work) -> 
+      let _ = foldleft(xs)(0)(fun (r)(x) -> (work(r)(x);r+1))
+  in
+  ();;
 let
 list_iforeach =
 fun xs -> foldleft_to_iforeach(list_foldleft)(xs)
