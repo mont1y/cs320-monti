1ef72bd2700333c9d6e693ec022dfc761ac6e43b
unknown <ryan.zhang126@gmail.com>
2023-10-16 16:38:52 -0400

    test

---
+++ assigns/assign4/MySolution/OCaml/assign4-1.ml
@@ -0,0 +1,9 @@
+#use "./../../../classlib/OCaml/MyOCaml.ml";; 
+let the_ln2_stream: float stream = fun() ->
+  let rec streamize(denom)(sum)(sign): float stream = fun() ->
+    let nextNum = float(sign) /. denom in
+    let newSum = sum +. nextNum in
+    StrCons(newSum, streamize(denom+.1.0)(newSum)(sign * -1))
+  in
+  (streamize 1.0 0.0 (1)) ()
+;;
