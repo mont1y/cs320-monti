69b338f53ca6ba731a123571cde49b323830051c
unknown <ryan.zhang126@gmail.com>
2023-10-16 17:54:00 -0400

    test

---
+++ assigns/assign4/MySolution/OCaml/assign4-2.ml
@@ -0,0 +1,11 @@
+let theNatPairs: (int * int) stream = fun () ->
+  let rec generate_pairs i j = fun () ->
+    StrCons((i, j), fun () ->
+      if i = 0 then
+        generate_pairs(j + 1)(i) ()
+      else
+        generate_pairs(i - 1)(j+1) ()
+    )
+  in
+  (generate_pairs (0)(0))  ()
+;;
