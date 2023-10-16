4ae386b7613a700e9918dcc213125376be248612
unknown <ryan.zhang126@gmail.com>
2023-10-16 16:45:04 -0400

    test

--- assigns/assign4/MySolution/OCaml/assign4-1.ml
+++ assigns/assign4/MySolution/OCaml/assign4-1.ml
@@ -1,4 +1,5 @@
-#use "./../../../classlib/OCaml/MyOCaml.ml";; 
+#use "./../../../../classlib/OCaml/MyOCaml.ml";;
+
 let the_ln2_stream: float stream = fun() ->
   let rec streamize(denom)(sum)(sign): float stream = fun() ->
     let nextNum = float(sign) /. denom in
